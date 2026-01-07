90f5c27ed2e6d8196ce4c7b58e1bac979a48bd36
notridani <musicodorpg@gmail.com>
2026-01-07 20:25:34 -0300

    agora o mapa do tabuleiro é uma arrat do objeto tile

--- tabuleiro/tabuleiro.py
+++ tabuleiro/tabuleiro.py
@@ -1,15 +1,29 @@
 import pygame
-from .tiles import criar_tiles
+from .tiles import tile
 from variaveis_globais import *
-from .mapas import mapas
+from .mapas import mapa
 
 
 class tabuleiro:
-    def __init__(self, posicao_tabuleiro, mapa, cor1, cor2: tuple | None = None):
+    def __init__(self, posicao_tabuleiro, mapa_base, cor1, cor2: tuple | None = None):
         self.posicao = (posicao_tabuleiro[0] * tamanho_dos_tiles, posicao_tabuleiro[1] * tamanho_dos_tiles)
-        self.mapa = mapas[mapa]
+
         self.cor1 = cor1
         self.cor2 = cor2
+        self.mapa_base = mapa[mapa_base]
+        self.tile1 = tile(cor[self.cor1])
+        self.tile2 = tile(cor[self.cor2]) if cor2 else None
+
+        self.mapa = []
+
+        for X, linha in enumerate(self.mapa_base):
+            coluna = []
+
+            for Y, casa in enumerate(linha):
+                coluna.append(tile(cor[cor1]))
+
+            self.mapa.append(coluna)
+
         self.surface = pygame.Surface(
             (
                 len(self.mapa[0]) * tamanho_dos_tiles,
@@ -19,54 +33,23 @@ class tabuleiro:
         )
 
 
+
+
     def desenhar(self, tela):
 
-        tiles1 = criar_tiles(cor[self.cor1])
+        for Y, linha in enumerate(self.mapa):
+            for X, casa in enumerate(linha):
+                self.surface.blit(casa.surface, (X * tamanho_dos_tiles, Y * tamanho_dos_tiles))
 
-        if self.cor2 is None:
-            for Y, linha in enumerate(self.mapa):
-                for X, tile in enumerate(linha):
-                    if tile[0] == 1:
-                        self.surface.blit(
-                            tiles1,
-                            ((tamanho_dos_tiles) * X,
-                             (tamanho_dos_tiles) * Y)
-                        )
-
-        if self.cor2 is not None:
-            tiles2 = criar_tiles(cor[self.cor2])
-
-            for Y, linha in enumerate(self.mapa):
-                for X, tile in enumerate(linha):
-                    if tile[0] == 1:
-                        if Y % 2 == 1:
-                            if X % 2 == 0:
-                                self.surface.blit(
-                                    tiles1,
-                                    ((tamanho_dos_tiles) * X,
-                                     (tamanho_dos_tiles) * Y)
-                                )
-                            else:
-                                self.surface.blit(
-                                    tiles2,
-                                    ((tamanho_dos_tiles) * X,
-                                     (tamanho_dos_tiles) * Y)
-                                )
-                        else:
-                            if X % 2 == 0:
-                                self.surface.blit(
-                                    tiles2,
-                                    ((tamanho_dos_tiles) * X,
-                                     (tamanho_dos_tiles) * Y)
-                                )
-                            else:
-                                self.surface.blit(
-                                    tiles1,
-                                    ((tamanho_dos_tiles) * X,
-                                     (tamanho_dos_tiles) * Y)
-                                )
 
         tela.blit(self.surface, self.posicao)
+
+    def alterar_mapa(self, cordenadas, estado):
+        self.cordenadas = cordenadas
+        self.estado = estado
+
+
+
 
 
 
--- tabuleiro/tiles.py
+++ tabuleiro/tiles.py
@@ -1,8 +1,17 @@
 import pygame
 from variaveis_globais import tamanho_dos_tiles
 
-def criar_tiles(cor):
-    quadrado = pygame.Surface((tamanho_dos_tiles, tamanho_dos_tiles*1.5), pygame.SRCALPHA)
-    pygame.draw.rect(quadrado, cor, (0, 0, tamanho_dos_tiles, tamanho_dos_tiles))
-    pygame.draw.rect(quadrado, (cor[0]/2, cor[1]/2, cor[2]/2), (0, tamanho_dos_tiles, tamanho_dos_tiles, tamanho_dos_tiles/2))
-    return quadrado
+class tile:
+    def __init__(self, cor, altura: int | None = None, estado: str | None = None):
+        self.surface = pygame.Surface((tamanho_dos_tiles, tamanho_dos_tiles*1.5), pygame.SRCALPHA)
+        self.rect = self.surface.get_rect()
+
+        self.cor = cor
+        self.altura = altura if altura else 0
+        self.estado = estado if estado else ""
+
+        pygame.draw.rect(self.surface, self.cor, (0, 0, tamanho_dos_tiles, tamanho_dos_tiles))
+
+        sombra = (self.cor[0] //2, self.cor[1]//2, self.cor[2]//2)
+        pygame.draw.rect(self.surface, sombra, (0, tamanho_dos_tiles, tamanho_dos_tiles, tamanho_dos_tiles / 2))
+
